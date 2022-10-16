c2cd0d4e1b3c2c9eb652849bed1e94bb05e1d602
Yumjong <yumjongeun@gmail.com>
2022-10-16 17:45:43 +0900

    Calendar while loop

--- src/CalendarMaxday.java
+++ src/CalendarMaxday.java
@@ -9,19 +9,34 @@ public class CalendarMaxday {
 
 	public static void main(String[] args) {
 		
+		String PROMPT = "cal>";
 		Scanner scanner = new Scanner(System.in);
 		CalendarMaxday cal = new CalendarMaxday();
 		
-		System.out.print("반복 횟수를 입력하세요:");
-		int repeat = scanner.nextInt();
-		
-		for(int i = 0; i < repeat; i++) {
-			System.out.print("달을 입력해주세요:");
-			   int month = scanner.nextInt();
+//		System.out.println("반복횟수를 입력해 주세요");
+//		int repeat = scanner.nextInt();
+//		
+//		for(int i = 0; i < repeat; i++) {
+//			System.out.print("달을 입력해주세요:");
+//			System.out.print(PROMPT);
+//			   int month = scanner.nextInt();
+//			
+//			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDayOfMonth(month));
+//		}
 			
+		int month = 1;
+		while(true) {
+			System.out.println("달을 입력해주세요:");
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if(month == -1) {
+				break;
+			}else if(month > 12) {
+				continue;
+			}
 			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDayOfMonth(month));
 		}
-		
+//		
 		System.out.println("Bye~~");
 	
 		scanner.close();
