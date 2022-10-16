0117c685043a087d47f959cdfafd2d9608ade89a
Yumjong <yumjongeun@gmail.com>
2022-10-16 17:14:17 +0900

    Update Calendar

--- src/CalendarMaxday.java
+++ src/CalendarMaxday.java
@@ -11,10 +11,19 @@ public class CalendarMaxday {
 		
 		Scanner scanner = new Scanner(System.in);
 		CalendarMaxday cal = new CalendarMaxday();
-		System.out.print("달을 입력해주세요:");
-		int month = scanner.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDayOfMonth(month));
+		System.out.print("반복 횟수를 입력하세요:");
+		int repeat = scanner.nextInt();
+		
+		for(int i = 0; i < repeat; i++) {
+			System.out.print("달을 입력해주세요:");
+			   int month = scanner.nextInt();
+			
+			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getMaxDayOfMonth(month));
+		}
+		
+		System.out.println("Bye~~");
+	
 		scanner.close();
 	}
 
